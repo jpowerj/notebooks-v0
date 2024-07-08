daca209e568ee6aa420a26dc15217ea3d1218404
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2024-07-07 22:34:27 -0400

    Quartz sync: Jul 7, 2024, 10:34 PM

--- content/index.md
+++ content/index.md
@@ -1,5 +1,5 @@
 ---
-title: Welcome to Quartz
+title: Welcome to Jeff's Brain Dump
 ---
 
 This is a blank Quartz installation.
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Jeff's Second Brain 💆",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jjacobs.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {

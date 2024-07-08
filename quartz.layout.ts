f64de9279c2891db72532eee1cc2db0807614411
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2024-07-08 00:45:14 -0400

    Quartz sync: Jul 8, 2024, 12:45 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,7 +29,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.RecentNotes({limit: 2}),
+    Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],

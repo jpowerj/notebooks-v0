4a68d48ef8916d5e287cc680bc7da30595d9b875
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2024-08-11 23:08:09 -0400

    Quartz sync: Aug 11, 2024, 11:08 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,6 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jpowerj/notebooks",
-      // "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }

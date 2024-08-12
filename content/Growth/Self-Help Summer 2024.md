3121ab9ef568a2a1d9d734ecaeb3f758289c4221
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2024-08-11 22:32:19 -0400

    Quartz sync: Aug 11, 2024, 10:32 PM

---
+++ content/Growth/Self-Help Summer 2024.md
@@ -0,0 +1,5 @@
+---
+title: Self-Help Summer 2024
+---
+Self-help books I've read so far:
+
--- quartz.config.ts
+++ quartz.config.ts
@@ -66,7 +66,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks(),
+      Plugin.CrawlLinks({ openLinksInNewTab: true }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
